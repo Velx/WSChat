f7c82d57df05f7eec664befdcd5864d0b8e84a5c
Velx <feofan139@hotmail.com>
2020-10-02 00:19:11 +0300

    Added view and templates for login and registration

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        print(1)
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            print(2)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            auth_login(request, user)
+            print('logined')
+            return redirect('chat')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
